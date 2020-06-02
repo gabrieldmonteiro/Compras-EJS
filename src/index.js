fdf36db4bbfd631725dc058ea5812075737edc62
Gabriel <gabrieldmonteiro@live.com>
2020-06-02 10:13:55 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const Compras = require("./models/Compras");
-let control = false;
 
 //.ENV
 dotenv.config();
@@ -26,14 +25,10 @@ app.use("/static", express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/compras", (req, res) => {  
-  if(control){
   Compras.find({}, (err, tasks) => {
     res.render("../src/views/compras.ejs", { todoTasks: tasks });
     control = false;
   });
-}else{
-  res.render("../src/views/auth.ejs");
-}
 });
 
 //POST
@@ -84,8 +79,7 @@ app.route("/remove/:id").get((req, res) => {
 let pw = "";
 app.post("/", (req, res) => {
   pw = req.body.txtPassword;
-  if (pw === process.env.PASSWORD) {    
-    control = true;
+  if (pw === process.env.PASSWORD) {       
     res.redirect("/compras");
   } else {
     res.redirect("/");
